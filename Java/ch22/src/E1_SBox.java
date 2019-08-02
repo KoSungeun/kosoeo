61caf658dbc45bded5d380b1020c9e977bccdfc1
kosoeo <kosoeo@gmail.com>
2019-08-02 19:25:26 +0900

    소켓

--- Java/ch22/src/E1_SBox.java
+++ Java/ch22/src/E1_SBox.java
@@ -1,5 +1,9 @@
 
 public class E1_SBox implements java.io.Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4974674026703147608L;
 	String s;
 
 	public E1_SBox(String s) {
