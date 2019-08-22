ee58122d8c740a3e593cd0d37ffb31458cfaa1de
kosoeo <kosoeo@gmail.com>
2019-08-22 20:05:04 +0900

    커밋

--- kakao/Solution.java
+++ kakao/Solution.java
@@ -1,14 +1,43 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
+        List<Music> musicList = new ArrayList()<>;
+        Map<String, Integer> genresPlays = new HashMap<>();
+        for(int i=0; i < genres.length; i++) {
+            genresPlays.put(genres[i], genresPlays.getOrDefault(genres[i], 0) + plays[i]);
+            musicList.add(new Music(i, genres[i], plays[i]));
+            
+        }
+
+
+        
+        Map<String, Integer> genresSort = new LinkedHashMap<>();
+        
+
+        genresPlays.entrySet()
+            .stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .forEachOrdered(e -> genresSort.put(e.getKey(), e.getValue()));
+
         return answer;
     }
 }
 
+
+
+class genres {
+
+}
+
 class Music {
     private int num;
-    private String genres;
     private int plays;
     
     public Music(int num, String genres, int plays) {
@@ -21,7 +50,7 @@ class Music {
         return this.num;
     }
 
-    public String geres() {
+    public String getGeres() {
         return this.genres;
     }
 
