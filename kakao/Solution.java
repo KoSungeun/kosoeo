02c62030dbc9b37a94fc893aac3092f29f9905a0
kosoeo <kosoeo@gmail.com>
2019-08-23 18:26:07 +0900

    javascript

--- kakao/Solution.java
+++ kakao/Solution.java
@@ -1,31 +1,41 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        List<Music> musicList = new ArrayList()<>;
+        int[] answer = new int[plays.length];
+        List<Music> musicList = new ArrayList<>();
         Map<String, Integer> genresPlays = new HashMap<>();
         for(int i=0; i < genres.length; i++) {
             genresPlays.put(genres[i], genresPlays.getOrDefault(genres[i], 0) + plays[i]);
             musicList.add(new Music(i, genres[i], plays[i]));
             
         }
-
-
         
+        
+
         Map<String, Integer> genresSort = new LinkedHashMap<>();
         
-
         genresPlays.entrySet()
             .stream()
             .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
             .forEachOrdered(e -> genresSort.put(e.getKey(), e.getValue()));
 
+        musicList.stream()
+        .sorted((a,b) ->  b.getPlays() - a.getPlays()).sorted().forEach(System.out::println);
+
+        for(String genresRank : genresSort.keySet()) {
+            for(Music music : musicList) {
+                if(music.getGeres().equals(genresRank)) {
+                    music.getNum();
+                }
+            }
+            
+        }
         return answer;
     }
 }
@@ -39,6 +49,7 @@ class genres {
 class Music {
     private int num;
     private int plays;
+    private String genres;
     
     public Music(int num, String genres, int plays) {
         this.num = num;
